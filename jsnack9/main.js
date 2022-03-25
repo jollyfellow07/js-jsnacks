721e6f70f4a746bbc91939ee2d9d8213451e8b50
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-25 18:24:10 +0100

    corretto esercizio 9

--- jsnack9/main.js
+++ jsnack9/main.js
@@ -1,23 +1,32 @@
 let contenitoreNumeri = [];
-alert("inserisci 10 numeri");
-//compongo il mio array
-for(let i = 0; i < 10;i++){
-    let numero =parseInt(prompt("inserisci il numero"));
-    contenitoreNumeri.push(numero);
-}
-let media = mediaAritmetica(contenitoreNumeri);
+let somma = addizione(contenitoreNumeri);
+let media = mediaAritmetica(somma);
 alert("la media aritmetica dei numeri è: " + media); 
 
 
-/*****LE MIE FUNZIONI*****/
-function mediaAritmetica (calcolo){
 
+/***********************************************************/
+
+/*****LE MIE FUNZIONI*****/
+function addizione(calcolo) {
 //sommo i numeri del mio array
+alert("inserisci 10 numeri");
+//compongo il mio array
+for(let i = 0; i < 10;i++){
+    let numero = parseInt(prompt("inserisci il numero"));
+    contenitoreNumeri.push(numero);
+}
 let somma = 0;
 for (let i = 0; i < calcolo.length; i++) {
     somma += calcolo[i];
+    }
+
+console.log(calcolo);
+return somma;
 }
-console.log(somma);
-let media = somma/10;
+
+function mediaAritmetica (addizione1){
+console.log(addizione1);
+let media = addizione1/10;
 return media;
 }
