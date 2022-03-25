73f28e653653edfca0e02ceb4d59a272e3c927b4
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-25 13:31:49 +0100

    esercizio numero 1 completato

---
+++ jsnack1/main.js
@@ -0,0 +1,12 @@
+let numero1 = parseInt(prompt("inserisci in numero 1"));
+let numero2 = parseInt(prompt("inserisci in numero 2"));
+if(numero1>numero2){
+
+alert("il numero maggiore è:" + numero1);
+    }else if(numero2>numero1){
+
+        alert("il numero maggiore è:" + numero2);
+       
+    }else{
+            alert(numero1 + " e " + numero2 + " sono uguali!")
+    }
