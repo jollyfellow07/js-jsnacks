85a929e2c8f53bae60973ee8fafafbfbb4736b5e
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-25 13:37:54 +0100

    Parola corta (es2) completato

---
+++ jsnack2/main.js
@@ -0,0 +1,12 @@
+let stringa1 = prompt("inserisci la prima parola");
+let stringa2 = prompt("inserisci la seconda parola");
+if(stringa1<stringa2){
+
+alert("la stringa più corta è:" + stringa1);
+    }else if(stringa2<stringa1){
+
+        alert("la stringa più corta è:" + stringa2);
+       
+    }else{
+            alert(stringa1 + " e " + stringa2 + " sono di lunghezza uguale!")
+    }
