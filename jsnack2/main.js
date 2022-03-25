93fbff0f4fb29d3b91ac04db8f6535e5dae113f7
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-25 13:39:36 +0100

    corretto esercizio 2

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -2,10 +2,13 @@ let stringa1 = prompt("inserisci la prima parola");
 let stringa2 = prompt("inserisci la seconda parola");
 if(stringa1<stringa2){
 
-alert("la stringa più corta è:" + stringa1);
+    alert("la stringa più corta è:" + stringa1);
+    alert("la stringa più lunga è:" + stringa2);
+
     }else if(stringa2<stringa1){
 
         alert("la stringa più corta è:" + stringa2);
+        alert("la stringa più lunga è:" + stringa1);
        
     }else{
             alert(stringa1 + " e " + stringa2 + " sono di lunghezza uguale!")
