9515db2dd6532386b970320dd752e02e7df117e0
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-26 18:31:31 +0100

    fixati problemi jsnack 6

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,11 +1,11 @@
 let condizione = false; 
-while(condizione==false){
 alert("prova la sorte e scrivi un numero, se il numero è uguale a quello che ha scelto il pc avrai vinto")
+while(condizione==false){
+
     let numeroPc = Math.round(Math.random()*10);
-    alert(numeroPc)
     //immetiamo il numero per controllare
     let numeroUtente = parseInt(prompt("inserisci un numero da 0 a 10"));
-
+    alert(numeroPc);
         if (numeroPc==numeroUtente){ //confronto numeri
             alert("hai vinto, avete scelto lo stesso numero!");
             condizione=true;
